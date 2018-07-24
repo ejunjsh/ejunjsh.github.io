df320a1f1ed614c2b0c21fce6d23d2cfff7855ae
zhouff <sjj050121014@163.com>
2018-07-24 21:46:47 +0800

    Site updated: 2018-07-24 21:46:45

---
+++ raft/raft/scripts/frames/title.js
@@ -0,0 +1,35 @@
+
+"use strict";
+/*jslint browser: true, nomen: true*/
+/*global define*/
+
+define([], function () {
+    return function (frame) {
+        var player = frame.player(),
+            layout = frame.layout();
+
+        frame.after(1, function() {
+            frame.model().clear();
+            layout.invalidate();
+        })
+
+        .after(500, function () {
+            frame.model().title = '<h1 style="visibility:visible">Raft</h1>'
+                        + '<h2 style="visibility:visible">Understandable Distributed Consensus</h2>'
+                        + '<br/>' + frame.model().controls.html();
+            layout.invalidate();
+        })
+        .after(500, function () {
+            frame.model().subtitle = '';
+            layout.invalidate();
+            frame.model().controls.show();
+        })
+
+
+        .after(100, function () {
+            player.next();
+        })
+        
+        player.play();
+    };
+});
